279066f82117345d5dd2a059f81d902977f09c7e
sashagalanix <sashagalanix@gmail.com>
2025-07-09 17:24:32 +0000

    base structure

---
+++ libs/common/src/events.ts
@@ -0,0 +1,4 @@
+import { FacebookEvent } from "./types/facebook";
+import { TiktokEvent } from "./types/tiktok";
+
+export type Event = FacebookEvent | TiktokEvent;
---
+++ libs/common/src/types/shared.ts
@@ -0,0 +1,1 @@
+export type FunnelStage = "top" | "bottom";
